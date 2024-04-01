fda10dc60a41402822fc0bb4bac3ceca0466bf18
Jonathan Ung <jonathan.keith.ung@gmail.com>
2024-04-01 14:04:50 -0700

    functional login and registration

--- client/app/calendar/page.tsx
+++ client/app/calendar/page.tsx
@@ -1,4 +1,21 @@
+"use client";
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import axios from 'axios';
 export default function Calendar() {
+    const router = useRouter();
+    useEffect(() => {
+        axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/user/current`, { withCredentials: true })
+            .then((response) => { 
+                if (response.status !== 200) {
+                    router.push('/');
+                } else {
+                    
+                }
+            }).catch((err) => { 
+                router.push('/');
+            });
+    }, []);
     return (
         <div>
             <p>Calendar</p>
